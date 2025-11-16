5418e19c4ad53a4eb75169f0923fb4e57735c6c0
jason cox <JAcox12@icloud.com>
2025-11-16 03:26:26 -0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+function BALLS() {
+  console.say("BALLS", native-french, AAVE);
+
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+  <button className="balls" onClick={BALLS}>balls</button>
         <a
           className="App-link"
           href="https://reactjs.org"
